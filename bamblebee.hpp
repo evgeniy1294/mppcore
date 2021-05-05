e04d233b040bc645d7522d278c9f266103f160f3
Evgenii Fedoseev <evgeniy1294@yandex.ru>
2021-05-05 23:32:25 +0300

    Настройка прерываний для ECLIC перенесена в core

--- bamblebee.hpp
+++ bamblebee.hpp
@@ -193,6 +193,61 @@ namespace mpp::core {
         template < class... Interrupt >
         constexpr static void DisableInterrupts() { ( Interrupt::Disable(), ... ); }
     };
+    
+    
+    
+    
+    
+    
+    enum class Trigger:    uint8_t { Level = 0b000, Posedge = 0b010, Negedge = 0b110 };
+    enum class HandleMode: uint8_t { NonVectored = 0b0, Vectored = 0b1 };
+    
+    
+    
+     struct IrqExampleTrait final {
+        constexpr static HandleMode   kHandleMode = HandleMode::NonVectored;
+        constexpr static Trigger      kTrigger    = Trigger::Level;
+        constexpr static std::uint8_t kPriorityLevel = 0b1101;
+     };
+    
+         
+    
+    template < IRQn_Type tInterruptSource, class Trait >
+    class Interrupt final {
+      public:
+        static_assert(::std::is_same_v< Trait, ::std::decay_t< decltype(Trait()) > >);
+        static_assert( tInterruptSource < ECLIC_NUM_INTERRUPTS, "Wrong interrupt source ID");
+        
+        constexpr static IRQn_Type    kInterruptSource = tInterruptSource;
+        constexpr static HandleMode   kHandleMode = Trait::kHandleMode;
+        constexpr static Trigger      kTrigger = Trait::kTrigger;
+        constexpr static std::uint8_t kPriorityLevel = Trait::kPriorityLevel;
+            
+        constexpr static std::uint8_t kIntAttrMask = static_cast< std::uint8_t >(kTrigger) |
+                                                     static_cast< std::uint8_t >(kHandleMode);
+        constexpr static std::uint8_t kIntCfgMask  = kPriorityLevel << (8 - ECLICINTCTLBITS);
+                              
+        inline static void Init() {
+          constexpr std::uintptr_t intcfg  = ECLIC_ADDR_BASE + ECLIC_INT_CTRL_OFFSET + static_cast< std::uintptr_t >(kInterruptSource) * 4;
+          constexpr std::uintptr_t intattr = ECLIC_ADDR_BASE + ECLIC_INT_ATTR_OFFSET + static_cast< std::uintptr_t >(kInterruptSource) * 4;
+          
+          *(volatile uint8_t*)intcfg  = kIntCfgMask;
+          *(volatile uint8_t*)intattr = kIntAttrMask;
+        }
+        
+        inline static void Enable() {
+          constexpr std::uintptr_t intie = ECLIC_ADDR_BASE + ECLIC_INT_IE_OFFSET + static_cast< std::uintptr_t >(kInterruptSource) * 4;
+          *(volatile uint8_t*)intie = 1; 
+        }
+          
+        inline static void Disable() {
+          constexpr std::uintptr_t intie = ECLIC_ADDR_BASE + ECLIC_INT_IE_OFFSET + static_cast< std::uintptr_t >(kInterruptSource) * 4;
+          *(volatile uint8_t*)intie = 0; 
+        }
+    };
+    
+    
+    
   } // inline namespace
 }
 
